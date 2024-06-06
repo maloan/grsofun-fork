4f5397bd4e906698e38c47054f52dd941038accb
Beni Stocker <benjamin.stocker@gmail.com>
2024-06-06 13:48:07 +0200

    successful test at single site: correct forcing, p-model runs

--- R/grsofun_run.R
+++ R/grsofun_run.R
@@ -65,7 +65,8 @@ grsofun_run_byilon <- function(ilon, par, settings){
   # That's where it all happens...
   message(paste("Running rsofun for ilon =", ilon))
 
-  # xxx test: for DE-Tha (DE-Tha  lon = 13.6, lat = 51.0, elv = 380 m), use ilon = 388 (lon = 13.75, lat = 50.75, sitename = grid_ilon_388_ilat_174)
+  # xxx test: ilon = 388
+  # for DE-Tha (DE-Tha  lon = 13.6, lat = 51.0, elv = 380 m), use ilon = 388 (lon = 13.75, lat = 50.75, sitename = grid_ilon_388_ilat_174)
 
   # get land mask - variable name hard coded
   df <- readr::read_rds(paste0(settings$dir_landmask_tidy, "WFDEI-elevation_ilon_", ilon, ".rds")) |>
@@ -103,6 +104,7 @@ grsofun_run_byilon <- function(ilon, par, settings){
       ) |>
 
       # convert units and rename
+      dplyr::rowwise() |>
       dplyr::mutate(
         Tair = Tair - 273.15,  # K -> deg C
         ppfd = SWdown * kfFEC * 1.0e-6,  # W m-2 -> mol m-2 s-1
@@ -118,7 +120,6 @@ grsofun_run_byilon <- function(ilon, par, settings){
         netrad = NA,
         ccov = 0.5,
         co2 = 400,
-        fapar = 1,
       ) |>
       dplyr::select(
         lon,
@@ -132,7 +133,6 @@ grsofun_run_byilon <- function(ilon, par, settings){
         ccov,
         snow = Snowf,
         co2,
-        fapar,
         patm = PSurf,
         tmin = Tair,
         tmax = Tair
@@ -141,18 +141,113 @@ grsofun_run_byilon <- function(ilon, par, settings){
       dplyr::group_by(lon, lat) |>
       tidyr::nest()
 
+    # # xxx test
+    # df_climate |>
+    #   filter(lat == 50.75) |>
+    #   unnest(data) |>
+    #   ggplot(aes(date, vpd)) +
+    #   geom_line()
+    #
+    # df_climate |>
+    #   filter(lat == 50.75) |>
+    #   unnest(data) |>
+    #   visdat::vis_miss()
+
   }
 
   if (settings$source_fapar == "modis"){
-    df <- read_rds(paste0(settings$dir_fapar_tidy, "MODIS-C006_MOD15A2_LAI_FPAR_zmaw_ilon_", ilon, ".rds")) |>
-      tidyr::unnest(data)
 
-    # match years read from climate
-    dates <- df_forcing$data[[1]]$date
-    year_end <- max(lubridate::year(dates[lubridate::month(dates) == 12 & lubridate::mday(dates) == 31]))
-    year_start <- min(lubridate::year(dates[lubridate::month(dates) == 1 & lubridate::mday(dates) == 1]))
+    # read monthly fAPAR data
+    df_fapar_mon <- read_rds(paste0(settings$dir_fapar_tidy, "MODIS-C006_MOD15A2_LAI_FPAR_zmaw_ilon_", ilon, ".rds")) |>
+      mutate(data = purrr::map(data, ~rename(., date = time))) |>
+
+      # xxx try
+      filter(lat == 50.75)
+
+    # df_fapar_mon$data[[1]] |>
+    #   ggplot(aes(date, fpar)) +
+    #   geom_line()
+
+    # linearly interpolate monthly fAPAR values to daily
+    dates <- df_fapar_mon$data[[1]]$date
+    year_start <- min(lubridate::year(dates[lubridate::month(dates) == 1]))
+    year_end <- max(lubridate::year(dates[lubridate::month(dates) == 12]))
+
+    ddf <- tibble(
+      date = seq(
+        from = lubridate::ymd(paste0(year_start, "-01-01")),
+        to = lubridate::ymd(paste0(year_end, "-12-31")),
+        by = "days"
+      ))
+
+    interpolate2daily_fpar <- function(df, ddf){
+      # linearly interpolate (leaves training NAs)
+      ddf <- ddf |>
+        dplyr::left_join(
+          df,
+          by = "date"
+        ) |>
+        dplyr::mutate(
+          fpar_daily = zoo::na.approx(fpar, na.rm = FALSE)
+        )
+
+      # fill remaining with mean seasonal cycle
+      meandf <- ddf |>
+        dplyr::mutate(doy = lubridate::yday(date)) |>
+        dplyr::group_by(doy) |>
+        dplyr::summarise(fpar_meandoy = mean(fpar_daily, na.rm = TRUE))
+
+      ddf <- ddf |>
+        dplyr::mutate(doy = lubridate::yday(date)) |>
+        dplyr::left_join(
+          meandf,
+          by = "doy"
+        ) |>
+        dplyr::mutate(fpar_daily = ifelse(is.na(fpar_daily), fpar_meandoy, fpar_daily)) |>
+        dplyr::select(-fpar_meandoy, -doy)
+
+      return(ddf)
+    }
+
+    df_fapar <- df_fapar_mon |>
+      mutate(data = purrr::map(data, ~interpolate2daily_fpar(., ddf))) |>
+      mutate(data = purrr::map(data, ~dplyr::select(., -fpar))) |>
+      mutate(data = purrr::map(data, ~dplyr::rename(., fapar = fpar_daily)))
+
+    # df_fapar$data[[1]] |>
+    #   ggplot(aes(date, fpar)) +
+    #   geom_line()
+
+    # # reduce to match years available from climate forcing
+    # dates <- df_climate$data[[1]]$date
+    # year_start <- min(lubridate::year(dates[lubridate::month(dates) == 1 & lubridate::mday(dates) == 1]))
+    # year_end <- max(lubridate::year(dates[lubridate::month(dates) == 12 & lubridate::mday(dates) == 31]))
+
 
   }
+
+  # combine to the most limiting time span among the climate and fapar time series
+  df_forcing <- df_climate |>
+    tidyr::unnest(data) |>
+    dplyr::inner_join(
+      df_fapar |>
+        tidyr::unnest(data),
+      by = c("lon", "lat", "date")
+    ) |>
+    dplyr::group_by(lon, lat) |>
+    tidyr::nest()
+
+  # # xxx test
+  # df_forcing |>
+  #   filter(lat == 50.75) |>
+  #   unnest(data) |>
+  #   ggplot(aes(date, vpd)) +
+  #   geom_line()
+  #
+  # df_forcing |>
+  #   filter(lat == 50.75) |>
+  #   unnest(data) |>
+  #   visdat::vis_miss()
 
   # populate driver object
   if (settings$model == "pmodel"){
@@ -161,7 +256,7 @@ grsofun_run_byilon <- function(ilon, par, settings){
 
       # merge with forcing time series
       left_join(
-        df_climate |>
+        df_forcing |>
           dplyr::rename(forcing = data),
         join_by(lon, lat)
       ) |>
@@ -221,6 +316,18 @@ grsofun_run_byilon <- function(ilon, par, settings){
       )
   }
 
+  # xxx test (near DE-Tha)
+  df |>
+    filter(sitename == "grid_ilon_388_ilat_174") |>
+    unnest(forcing) |>
+    ggplot(aes(date, vpd)) +
+    geom_line()
+
+  df |>
+    filter(sitename == "grid_ilon_388_ilat_174") |>
+    unnest(forcing) |>
+    visdat::vis_miss()
+
   # run model
   out <- rsofun::runread_pmodel_f(
     drivers = df |>
@@ -228,7 +335,11 @@ grsofun_run_byilon <- function(ilon, par, settings){
     par = par
   )
 
-  # xxx test (near DE-Tha)
+  # xxx test
+  out$data[[1]] |>
+    ggplot(aes(date, fapar)) +
+    geom_line()
+
   out$data[[1]] |>
     ggplot(aes(date, gpp)) +
     geom_line()
